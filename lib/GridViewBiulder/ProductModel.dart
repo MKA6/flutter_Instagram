61ed78472a9d6b4e2fcd6aae759cd0deb8fcdd40
mohammed_almasri <mohhammmedalmasri20@gmail.com>
2022-11-26 13:40:38 +0200

    .

--- lib/GridViewBiulder/ProductModel.dart
+++
@@ -1,8 +0,0 @@
-class ProductModel{
-  String name;
-  String desc;
-  String price;
-  String image;
-
-  ProductModel({required this.name,required this.desc,required this.price,required this.image});
-}
